46ba57ebf836a192ac67f9bb7ca79f7440dbb73e
Karan Singh <karansinghkundan@gmail.com>
2019-08-09 16:50:24 -0400

    implementing cardview

--- app/src/main/java/com/example/c0754427_mad3125_midterm/DataStore.java
+++ app/src/main/java/com/example/c0754427_mad3125_midterm/DataStore.java
@@ -1,6 +1,11 @@
 package com.example.c0754427_mad3125_midterm;
 
 
+
+
+//code example from moodle day 7 file
+
+
         import android.content.Context;
 
         import org.json.JSONArray;
@@ -34,7 +39,6 @@ package com.example.c0754427_mad3125_midterm;
         byte[] buffer = new byte[size];
         int count = is.read(buffer);
         is.close();
-        //Log.d("-- COUNT --", String.format("%d Bytes",count));
         json = new String(buffer, "UTF8");
         } catch (IOException ex) {
         ex.printStackTrace();
@@ -56,8 +60,6 @@ package com.example.c0754427_mad3125_midterm;
 
         SpaceXFlight mSpaceXFlight = getFlightObjectFromJSON(mJSONArray.getJSONObject(i));
         mSpaceXFlightList.add(mSpaceXFlight);
-        //Log.d("-- JSON -- ", mSpaceXFlight.toString());
-
         }
         }
         catch(JSONException e)
@@ -75,28 +77,20 @@ package com.example.c0754427_mad3125_midterm;
         String launch_year = userJsonObject.getString("launch_year");
         String launch_window = userJsonObject.getString("launch_window");
         String details = userJsonObject.getString("details");
-
-        //Read Rocket
         JSONObject rocket = new JSONObject(userJsonObject.getJSONObject("rocket").toString());
         String rocket_id = rocket.getString("rocket_id");
         String rocket_name = rocket.getString("rocket_name");
         String rocket_type = rocket.getString("rocket_type");
-
-        //Read Launch Site
         JSONObject launchSite = new JSONObject(userJsonObject.getJSONObject("launch_site").toString());
         String site_id = launchSite.getString("site_id");
         String site_name = launchSite.getString("site_name");
         String site_name_long = launchSite.getString("site_name_long");
-
-        //Read Links
         JSONObject links = new JSONObject(userJsonObject.getJSONObject("links").toString());
         String mission_patch = links.getString("mission_patch");
         String mission_patch_small = links.getString("mission_patch_small");
         String article_link = links.getString("article_link");
         String wikipedia = links.getString("wikipedia");
         String video_link = links.getString("video_link");
-
-        //start creating User object
         SpaceXFlight mSpaceXFlight = new SpaceXFlight();
         mSpaceXFlight.setFlight_number(flight_number);
         mSpaceXFlight.setMission_name(mission_name);
@@ -104,19 +98,16 @@ package com.example.c0754427_mad3125_midterm;
         mSpaceXFlight.setLaunch_year(launch_year);
         mSpaceXFlight.setLaunch_window(launch_window);
         mSpaceXFlight.setDetails(details);
-
         Rocket mRocket = new Rocket();
         mRocket.setRocket_id(rocket_id);
         mRocket.setRocket_name(rocket_name);
         mRocket.setRocket_type(rocket_type);
         mSpaceXFlight.setRocket(mRocket);
-
         LaunchSite mLaunchSite = new LaunchSite();
         mLaunchSite.setSite_id(site_id);
         mLaunchSite.setSite_name(site_name);
         mLaunchSite.setSite_name_long(site_name_long);
         mSpaceXFlight.setLaunchSite(mLaunchSite);
-
         Links mLinks = new Links();
         mLinks.setMission_patch(mission_patch);
         mLinks.setMission_patch_small(mission_patch_small);
